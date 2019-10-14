0d081a61b2b78835ef12cd7abed3a37ae6d8f375
Dale McConachie <dmcconac@umich.edu>
2019-10-14 13:50:23 -0400

    Finished parallel parsing framework.

--- path_execution/test_planning_results.m
+++ path_execution/test_planning_results.m
@@ -4,14 +4,13 @@ outputdir = "/mnt/big_narstie_data/dmcconac/car_planning_classification_experime
 [~, ~, ~] = mkdir(outputdir);
 files = dir(append(basedir, "*path.csv"));
 filenames = sort({files.name});
-filenames = filenames(1:2);
 
 car = make_car(false);
 
-aggregated_transition_distances = cell(2, length(filenames));
+transition_data = cell(2, length(filenames));
 
 tic
-for idx = 1:length(filenames)
+parfor idx = 1:length(filenames)
     file = filenames(idx);
     path_file = append(basedir, file{1});
     obstacles_file = append(basedir, file{1}(1:end-8), "obstacles.csv");
@@ -27,10 +26,22 @@ for idx = 1:length(filenames)
     features = generate_features(car, obstacles, executed_waypoints);
     transition_distances = generate_distances(obstacles, executed_waypoints, executed_states);
     
-    aggregated_transition_distances(:, idx) = {features, transition_distances};
+    transition_data(:, idx) = {features, transition_distances};
 
     fprintf("Parsing %s, path contains %d waypoints, result contains %d valid waypoints\n", ...
         file{1}(length(basedir):end), length(waypoints), last_valid_waypoint_idx);
 end
-% writematrix(aggregated_transition_distances', append(outputdir, "distances.csv"));
 toc
+
+%%
+save(append(outputdir, 'transition_data.mat'), 'transition_data');
+aggregated_transitions = [transition_data{1, :}];
+aggregated_transition_distances = [transition_data{2, :}];
+clearvars transition_data
+
+parfor idx = 1:length(aggregated_transitions)
+    outfile_name = append(outputdir, sprintf("%d.png", idx - 1));    
+    save_to_file(aggregated_transitions(idx), outfile_name);
+end
+
+writematrix(aggregated_transition_distances', append(outputdir, "distances.csv"));
